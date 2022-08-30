ab0dfa77a4d0ed4f281ce3301200b316c5b60566
narba <narba@DESKTOP-KRRRREC>
2022-08-30 11:26:07 -0300

    Add project files.

---
+++ EFCore.WebApi/Models/Arma.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore.WebApi.Models
+{
+    public class Arma
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public Heroi Heroi { get; set; }
+        public Guid HeroiId { get; set; }
+    }
+}
---
+++ EFCore.WebApi/Models/Heroi.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore.WebApi.Models
+{
+    public class Heroi
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public Batalha Batalha { get; set; }
+        public Guid BatalhaId { get; set; }
+
+    }
+}
