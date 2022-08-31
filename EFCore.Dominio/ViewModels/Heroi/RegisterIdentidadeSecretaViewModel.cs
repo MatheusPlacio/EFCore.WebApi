f97b81d7cc2449c5bbca0410223ce36a083ac946
narba <narba@DESKTOP-KRRRREC>
2022-08-31 17:04:47 -0300

    HeroisController

---
+++ EFCore.Dominio/ViewModels/Heroi/RegisterIdentidadeSecretaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Dominio.ViewModels.Heroi
+{
+    public class RegisterIdentidadeSecretaViewModel
+    {
+        public int Id { get; set; }
+        public string NomeReal { get; set; }
+        public int HeroiId { get; set; }
+      
+    }
+}
--- EFCore.WebApi/Controllers/HeroisController.cs
+++ EFCore.WebApi/Controllers/HeroisController.cs
@@ -1,9 +1,9 @@
 ﻿using EFCore.Dominio.Models;
 using EFCore.Dominio.ViewModels.Heroi;
 using EFCore.Repo.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EFCore.WebApi.Controllers
 {
@@ -30,23 +30,57 @@ namespace EFCore.WebApi.Controllers
             return await _context.Herois.FirstOrDefaultAsync(a => a.Id == id);
         }
 
+        //var HeroiNome = _context.IdentidadesSecretas
+        //    .Include(h => h.HeroiId)
+        //    .Where(h => h.HeroiId == id)
+        //    .Include(x => x.Heroi)
+        //    .Where(x => x.Heroi.Nome == nomeReal)
+        //    .FirstOrDefault();
+
         [HttpPost]
-        public ActionResult Post(RegisterHeroiViewModel novoHeroi, int id)
+        public ActionResult Post(RegisterHeroiViewModel novoHeroi, int id, string nomeReal)
         {
             var h = new Heroi
-            {
-                Nome = novoHeroi.Nome,
-                Id = id 
+            {   
+                Nome = "Magneto",
+                Identidade = new IdentidadeSecreta
+                {
+                    HeroiId = id,
+                    NomeReal = nomeReal,
+                }
             };
+
             _context.Herois.Add(h);
             _context.SaveChanges();
             return Ok();
         }
 
-        [HttpPut]
-        public ActionResult Put(Heroi heroi)
+        [HttpPost("Armas")]
+        public ActionResult PostArmas(RegisterHeroiViewModel hero, int id)
         {
-            _context.Herois.Update(heroi);
+            var heroi = new Heroi
+            {
+                Nome = "Magneto",
+                Armas = new List<Arma>
+                {
+                    new Arma { Nome = "Mente"}                             
+                }
+            };
+            _context.Herois.Add(heroi);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+
+
+        [HttpPut]
+        public ActionResult Put(int id, string nome)
+        {
+            var heroi = _context.Herois
+                 .Where(h => h.Id == id)
+                 .FirstOrDefault();
+            heroi.Nome = nome;
+
             _context.SaveChanges();
 
             return Ok();
